e61706b701913b322d49aac020f8dae7c2f68ec3
san9w9n <san9w9n@naver.com>
2021-08-30 20:58:22 +0900

    2021-08-30

---
+++ 7weeks/3046.cpp
@@ -0,0 +1,11 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main(void) {
+    ios::sync_with_stdio(0); cin.tie(0);
+    int a, avg;
+    cin >> a >> avg;
+
+    cout << 2*avg - a;
+    return 0;
+}
